f2ce12b18fb4c56e334dd1c153aa18340aa4871a
Tim <64743416+timltj@users.noreply.github.com>
2020-05-24 15:34:27 +0800

    Updated from latest working prototype 1

--- NUSGoPracticeBot/Main.java
+++ NUSGoPracticeBot/Main.java
@@ -1,17 +1,17 @@
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
     public static void main(String[] args) {
-        ApiContextInitializer.init(); // init API context
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(); // create new bot API 
+        ApiContextInitializer.init(); // init api context
+        TelegramBotsApi botsApi = new TelegramBotsApi(); // create new bot api
 
         try {
-            telegramBotsApi.registerBot(new NUSGoPracticeBot());
+            botsApi.registerBot(new NUSGoBot());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-        
+        System.out.println("NUSGoBot successfully started!");
     }
 }
